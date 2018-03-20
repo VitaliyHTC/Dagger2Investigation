0d7b624398dc00df55a8e35ac8dd494114ef35bb
VitaliyHTC <vitaliyhtc@gmail.com>
2018-03-20 12:51:47 +0200

    Config class with constants implemented in Kotlin

---
+++ app/src/main/java/com/vitaliyhtc/dagger2investigation/Config.kt
@@ -0,0 +1,10 @@
+package com.vitaliyhtc.dagger2investigation
+
+object Config {
+
+    const val PRODUCTS_PER_PAGE: Int = 40
+    const val PRODUCTS_WHERE_NOT: String = "is_dead"
+
+    const val NO_VALUE_INT: Int = -1
+
+}
