d6589e0f610da187c2964060ac477e9350bcd1a8
VitaliyHTC <vitaliyhtc@gmail.com>
2018-03-21 18:26:19 +0200

    presentation scope interfaces to annotation classes > Kotlin

---
+++ app/src/main/java/com/vitaliyhtc/dagger2investigation/presentation/productdetails/di/ProductDetailsActivityScope.kt
@@ -0,0 +1,7 @@
+package com.vitaliyhtc.dagger2investigation.presentation.productdetails.di
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ProductDetailsActivityScope
