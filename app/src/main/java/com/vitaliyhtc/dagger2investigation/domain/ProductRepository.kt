575e270b507dd8fa63d049f22811af0b9b6dc260
VitaliyHTC <vitaliyhtc@gmail.com>
2018-03-21 17:18:55 +0200

    ProductRepository interface > Kotlin

---
+++ app/src/main/java/com/vitaliyhtc/dagger2investigation/domain/ProductRepository.kt
@@ -0,0 +1,11 @@
+package com.vitaliyhtc.dagger2investigation.domain
+
+import com.vitaliyhtc.dagger2investigation.domain.model.Product
+import io.reactivex.Single
+
+interface ProductRepository {
+
+    fun getProducts(page: Int): Single<List<Product>>
+
+    fun getOneProduct(productId: Int): Single<Product>
+}
